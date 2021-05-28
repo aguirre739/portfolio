b0f7c068cd8da6979c12d9d19b3a6c868c14e14a
aguirre739 <pame11_@live.com>
2021-05-28 09:01:03 -0300

    se agrego material-ui icons

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,9 +1,22 @@
-import "./topbar.scss"
+import "./topbar.scss";
+import {Person, } from "@material-ui/icons";
 
 export default function Topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">Pamela Aguirre</a>
+                    <div className="itemContainer">
+                        <Person></Person>
+                        <span>+54 9 3816676579</span>
+                    </div>
+                </div>
+                
+                <div className="right">
+                    lado derecho
+                </div>
+            </div>
         </div>
     )
 }
