6cfda1ec20cb1728b3a587dd7ad83bca1fa45e75
aguirre739 <pame11_@live.com>
2021-05-30 10:58:45 -0300

    se agrego intro

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,9 +1,38 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
-    return (
-        <div className="intro" id="intro">
-            
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:  60,
+      strings: ["Developer", "Designer"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/foto.png" alt="foto de perfil" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Pamela Aguirre</h1>
+          <h3>
+            Software <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/arrow-down.svg" alt="flecha abajo" />
+        </a>
+      </div>
+    </div>
+  );
 }
